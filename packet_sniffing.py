32ddca5076df4c7ec2cbed70889c204356f798eb
karthika <35093289+KarthikaNadarajan@users.noreply.github.com>
2021-08-12 14:49:10 +0530

    Add files via upload

---
+++ packet_sniffing.py
@@ -0,0 +1,14 @@
+# from socket import *
+import socket
+import struct
+import binascii
+s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x800))
+
+while True:
+   packet = s.recvfrom(2048)
+ethernet_header = packet[0][0:14]
+eth_header = struct.unpack("!6s6s2s", ethernet_header)
+print("Destination MAC:" + binascii.hexlify(eth_header[0]) + " Source MAC:" + binascii.hexlify(eth_header[1]) + " Type:" + binascii.hexlify(eth_header[2]))
+ipheader = pkt[0][14:34]
+ip_header = struct.unpack("!12s4s4s", ipheader)
+print("Source IP:" + socket.inet_ntoa(ip_header[1]) + " Destination IP:" + socket.inet_ntoa(ip_header[2]))
